76b7ebbb59c26ec3b87829d7a729bf5e2e04d214
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2023-03-18 18:23:15 +0800

    Added catppuccin theme for doom emacs

--- dotfiles/config/doom.d/config.el
+++ dotfiles/config/doom.d/config.el
@@ -1,3 +1,6 @@
+;; Set catppuccin theme
+(setq doom-theme 'catppuccin)
+
 ;; Set up jetbrains mono as default font
 (setq doom-font (font-spec :family "JetBrainsMono" :size 12 :weight 'light)
       doom-variable-pitch-font (font-spec :family "JetBrainsMono" :size 13)
