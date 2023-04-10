58a8a2f86aa4a3a33de1bc09b9313c7996224baa
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2023-04-11 07:15:19 +0800

    Remove: latex preview for emacs

--- dotfiles/config/doom.d/packages.el
+++ dotfiles/config/doom.d/packages.el
@@ -12,8 +12,6 @@
 ;; Install nov
 (package! nov)
 
-(package! latex-preview-pane)
-
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
