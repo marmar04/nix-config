34f556f56e34e0d575e5f3a879f09937d650087a
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2023-06-26 17:30:17 +0800

    add: symlinks for flatpak to recognise system fonts

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,6 +76,7 @@
       })
       */
     ];
+
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
@@ -278,6 +279,24 @@
   };
 
   # For flatpak
+  system.fsPackages = [ pkgs.bindfs ];
+  fileSystems = let
+    mkRoSymBind = path: {
+      device = path;
+      fsType = "fuse.bindfs";
+      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+    };
+    aggregatedFonts = pkgs.buildEnv {
+      name = "system-fonts";
+      paths = config.fonts.fonts;
+      pathsToLink = [ "/share/fonts" ];
+    };
+  in {
+    # Create an FHS mount to support flatpak host icons/fonts
+    "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
+    "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
+  };
+
   xdg.portal.enable = true;
 
   # Fonts
