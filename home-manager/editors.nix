3f59ea718cfbfc1951d1b72cdfa70f50504d0c57
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2023-08-29 21:05:17 +0800

    remove: nvim

--- home-manager/editors.nix
+++ home-manager/editors.nix
@@ -49,54 +49,6 @@
   };
 
   programs = {
-    # neovim
-    neovim = {
-      enable = true;
-      # package = pkgs.neovim-nightly;
-      extraConfig = ''
-        ${builtins.readFile ./../dotfiles/config/nvim/init.vim}
-        lua << EOF
-        ${builtins.readFile ./../dotfiles/config/nvim/init.lua}
-      '';
-      plugins = with pkgs.vimPlugins; [
-        vim-addon-nix
-        nvim-lspconfig
-        nvim-cmp
-        cmp-buffer
-        cmp-path
-        cmp-spell
-        # (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
-        nvim-treesitter.withAllGrammars
-        # nvim-treesitter
-        cmp-treesitter
-        orgmode
-        onedark-nvim
-        catppuccin-nvim
-        neoformat
-        vim-nix
-        cmp-nvim-lsp
-        barbar-nvim
-        nvim-web-devicons
-        vim-airline
-        vim-ccls
-        vim-airline-themes
-        nvim-autopairs
-        neorg
-        vim-markdown
-        rust-tools-nvim
-        lspkind-nvim
-      ];
-      extraPackages = with pkgs; [
-        gcc
-        vimPlugins.packer-nvim
-        ripgrep
-        fd
-        nodePackages.pyright
-        nodePackages.eslint
-        ccls
-      ];
-    };
-
     # Vim
     vim = {
       enable = true;
