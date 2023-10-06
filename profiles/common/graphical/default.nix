85c77b1bb106d0b35801223ccf0e95038ab8d9a9
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2023-10-06 23:58:27 +0800

    change: default browser for captive-browser

--- profiles/common/graphical/default.nix
+++ profiles/common/graphical/default.nix
@@ -133,6 +133,18 @@
     captive-browser = {
       enable = true;
       interface = "wlo1";
+      browser = lib.concatStringsSep " " [
+        ''env XDG_CONFIG_HOME="$PREV_CONFIG_HOME"''
+        ''${pkgs.ungoogled-chromium}/bin/chromium''
+        ''--user-data-dir=''${XDG_DATA_HOME:-$HOME/.local/share}/chromium-captive''
+        ''--proxy-server="socks5://$PROXY"''
+        ''--host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE localhost"''
+        ''--no-first-run''
+        ''--new-window''
+        ''--incognito''
+        ''-no-default-browser-check''
+        ''http://cache.nixos.org/''
+      ];
     };
   };
 
