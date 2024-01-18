2669417960c9dc9880654dc956b448558514fbd8
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2024-01-18 21:16:20 +0800

    add: vscodium

--- home-manager/editors.nix
+++ home-manager/editors.nix
@@ -67,9 +67,14 @@
     # astroNvim
     # astronvim.enable = true;
 
-    # Emacs
-    emacs = {
-      enable = false;
+    vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+      ];
     };
   };
 
