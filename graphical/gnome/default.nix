47897ad46e83dbd41106b5db78e44d646e508bf2
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2024-01-21 07:55:00 +0800

    format: graphical/gnome/

--- graphical/gnome/default.nix
+++ graphical/gnome/default.nix
@@ -31,49 +31,50 @@
     udev.packages = with pkgs; [gnome.gnome-settings-daemon];
   };
 
-  environment.systemPackages = (with pkgs; [
-    # themes
-    adw-gtk3
-    adwaita-qt
-    gradience
+  environment.systemPackages =
+    (with pkgs; [
+      # themes
+      adw-gtk3
+      adwaita-qt
+      gradience
 
-    # gnome circle
-    shortwave
-    gnome-podcasts
-    shotwell
-    newsflash
-    foliate
-    amberol
-    blanket
-    citations
-    dialect
-    wike
-    junction
-    komikku
-    mousai
-    gnome-solanum
-    curtail
+      # gnome circle
+      shortwave
+      gnome-podcasts
+      shotwell
+      newsflash
+      foliate
+      amberol
+      blanket
+      citations
+      dialect
+      wike
+      junction
+      komikku
+      mousai
+      gnome-solanum
+      curtail
 
-    # socials
-    dino
-    tuba
-    #fractal-next # leave to compile overnight
-  ]) ++
-  (with pkgs.gnome; [
-    gnome-tweaks
-    pomodoro
-    polari
-    gnome-boxes
-    gnome-sudoku
-  ]) ++
-  (with pkgs.gnomeExtensions; [
-    appindicator
-    dash-to-panel
-    arcmenu
-    just-perfection
-    rounded-window-corners
-    clipboard-indicator
-  ]);
+      # socials
+      dino
+      tuba
+      #fractal-next # leave to compile overnight
+    ])
+    ++ (with pkgs.gnome; [
+      gnome-tweaks
+      pomodoro
+      polari
+      gnome-boxes
+      gnome-sudoku
+    ])
+    ++ (with pkgs.gnomeExtensions; [
+      appindicator
+      dash-to-panel
+      arcmenu
+      just-perfection
+      rounded-window-corners
+      clipboard-indicator
+    ]);
 
   programs = {
     kdeconnect = {
