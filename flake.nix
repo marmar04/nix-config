1ebf0b1575bcb5a643b39a24e18229dd0d3adf93
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2024-02-17 19:03:51 +0800

    add: niri flake

--- flake.nix
+++ flake.nix
@@ -39,10 +39,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hy3 = {
-      url = "github:outfoxxed/hy3";
-      inputs.hyprland.follows = "hyprland";
-    };
+    # window manager
+    niri.url = "github:sodiboo/niri-flake";
 
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
@@ -74,7 +72,7 @@
 
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
-    nix-colors.url = "github:misterio77/nix-colors";
+    #nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = {
