4fc1ff5517eee977721b73a33dfcd0b2287a0d13
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2024-03-10 22:22:59 +0800

    add: org-roam config

--- dotfiles/config/emacs/init.el
+++ dotfiles/config/emacs/init.el
@@ -63,6 +63,18 @@
 ;; Just some startup code for now
 (use-package org-journal)
 
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/Documents/roam_notes")
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (org-roam-setup))
+
 ;;;              _                                  _      _
 ;;;   __ _ _   _| |_ ___   ___ ___  _ __ ___  _ __ | | ___| |_ ___
 ;;;  / _` | | | | __/ _ \ / __/ _ \| '_ ` _ \| '_ \| |/ _ \ __/ _ \
