519d66cab4bfb305e0649b59ce157f42f4aa5054
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2024-04-17 07:29:22 +0800

    add: import container module to database

--- profiles/coding/database/default.nix
+++ profiles/coding/database/default.nix
@@ -25,6 +25,10 @@
     ];
   };
 
+  imports = [
+    ./../../containers/default.nix
+  ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
