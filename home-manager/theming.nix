61a9fc6795595d32ef22712554b8018554555012
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2025-03-27 16:22:57 +0800

    feat: remove nixpkgs overlay for theming

--- home-manager/theming.nix
+++ home-manager/theming.nix
@@ -16,42 +16,5 @@
     # Feel free to split up your configuration and import pieces of it here.
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      # outputs.overlays.additions
-      # outputs.overlays.modifications
-      # outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # (import self.inputs.emacs-overlay)
-
-      /*
-      (final: prev: {
-        catppuccin-kde = final.catppuccin-kde.overrideAttrs (oldAttrs: {
-          patches = [];
-        });
-      })
-      */
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    # config = {
-    # Disable if you don't want unfree packages
-    # allowUnfree = true;
-    # Workaround for https://github.com/nix-community/home-manager/issues/2942
-    # allowUnfreePredicate = _: true;
-    # };
-  };
-
   # TODO: Define _all_ themes and icons here
 }
