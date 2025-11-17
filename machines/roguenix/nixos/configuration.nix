5a8b3eb486fbab98fb245b1f583b899607bfb58b
Muhammad Ameer Rafiqi <m.ameerrafiqi@gmail.com>
2025-11-17 08:57:12 +0800

    feat: add python profile to roguenix

--- machines/roguenix/nixos/configuration.nix
+++ machines/roguenix/nixos/configuration.nix
@@ -23,6 +23,7 @@
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
     ./../../common/optimus.nix
+    ./../../../profiles/coding/python
     ./../../../profiles/coding/cpp
     ./../../../profiles/coding/java
     ./../../../profiles/coding/rust
